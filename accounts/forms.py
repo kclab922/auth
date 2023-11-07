a2fbc466269aab8821eb5b76c3c7578580ef16c0
changhee <och8808@gmail.com>
2023-11-07 11:29:56 +0900

    Signup 기능 구현

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields
